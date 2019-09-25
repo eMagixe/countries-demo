06fc4620f9fb71af9781ea5b5994c1bba3fa8885
eMagixe <emax.mails@gmail.com>
2019-09-25 22:34:47 +0500

    Create upload data in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        name: 'Max'
+        proxi: 'https://cors-anywhere.herokuapp.com/',
+        api: 'https://travelus.fun/api/v1/countries',
+        posts: []
+    },
+    getters: {
+        getPosts: (state) => {
+            return state.posts
+        }
+    },
+    mutations: {
+        setPosts (state, posts) {
+            state.posts = posts
+        }
+    },
+    actions: {
+        uploadData ({commit,state}) {
+            axios
+                .get(state.proxi + state.api)
+                .then(response => {
+                        commit('setPosts', response.data.data)
+                    })
+                .catch(error => {
+                    console.log(error);
+                    })
+        }
     }
 })
